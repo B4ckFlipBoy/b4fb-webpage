9f9ee4398d76627f6e4bfaba480be4b9ce3894f8
Philip Horn <Philip.Horn@md.de>
2018-09-07 15:14:28 +0200

    building start-page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,23 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Header></Header>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import Header from './components/Header.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      Header
+    },
+    data: () => {
+      return {
+        message: "Hello"
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    padding: 5px 0 5px 0;
+    background-color: dimgray;
+  }
+  .white{
+    color: #f3f3f3;
+  }
+  .white:hover{
+    color: #aeaeae;
+  }
 </style>
---
+++ vue/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="Header">
+      <div class="grid-x">
+        <div class="small-3 h3 border-right no-padding-bottom"><a href="#" class="white v-middle">B4fb.de</a></div>
+        <div class="small-2 h5 white underlined no-padding-bottom border-right"><p class="no-padding-bottom"><a href="https://www.b4fb.de/gg-gangster" class="white">GGG</a></p></div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+  #Header>div>div>p{
+    position: relative;
+    top: 10%;
+    margin-bottom: 0;
+    padding-bottom: 0;
+  }
+  .v-middle{
+    vertical-align: baseline;
+  }
+  .no-padding-bottom{
+    padding-bottom: 0;
+    margin-bottom: 0;
+  }
+  .border-right{
+    border-right: 2px solid #555555;
+  }
+  .underlined{
+    text-decoration: underline;
+  }
+</style>
